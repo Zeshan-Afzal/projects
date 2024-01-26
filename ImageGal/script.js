76535ac505dfd9e12f1d497dd85642aab5256e46
Zeshan Afzal <144064482+Zeshan-Afzal@users.noreply.github.com>
2024-01-27 00:33:22 +0500

    Update script.js

--- ImageGal/script.js
+++ ImageGal/script.js
@@ -1,15 +1,19 @@
 // Define the API URL
-const baseUrl = `https://api.unsplash.com/search/photos?per_page=144&page=10&query=`;
+
 //flag to check if the user LoggedIn or not
 let isLoggedIn = false;
+// pages for api
+let page=1;
 //api key
-let key = "&client_id=ds9Xs3l6PjIhSsb8NrDn9nTPYq1cvz90ZbOkqkS0RKs";
+let key = "";
 //variables to hold  data
 let query;
 let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 let gallery = JSON.parse(localStorage.getItem("gallery")) || [];
 //getting DOM elements
 const mainPhotosContainer = document.querySelector(".main-photo-container");
+let messageForPhotos=document.querySelector('.h1-for-photos')
+let LoadMoreBtn=document.querySelector('.more-btn ')
 let inputVal = document.querySelector(".inpVal");
 let tabs = document.querySelectorAll("li");
 let login = document.querySelector(".login");
@@ -24,21 +28,90 @@ login.addEventListener("click", (e) => {
 tabs.forEach((tab) => {
   tab.addEventListener("click", handleTabs);
 });
+//eventListner for loadMore button 
+LoadMoreBtn.addEventListener('click',()=>{
+	page++
+	searchAndRenderPhotos()
+})
+
+//
+inputVal.addEventListener('keydown',(e)=>{
+	if(e.key==='Enter'){
+		page=1;
+	  searchAndRenderPhotos()
+	}
+})
 //eventListner on search button to search for photos
+
 document
   .querySelector(".search-btn")
-  .addEventListener("click", searchAndRenderPhotos);
+  .addEventListener("click",()=>{
+	  
+	  page=1;
+	  searchAndRenderPhotos()
+  } );
 //eventListner on mainPhotosContainer to add remove photos
 document
   .querySelector(".main-photo-container")
   .addEventListener("click", handleAddRemoveClicks);
-//function responsible for handling tabs
+//function responsible for handling tabs and rendering photos on tab clicks
 function handleTabs(e) {
   let tabId = e.target.id;
   openTab(tabId);
+  let activeTabId=tabId;
 
-  searchAndRenderPhotos();
+  if (activeTabId === "main") {
+   searchAndRenderPhotos()
+  }
+
+  if (activeTabId === "gallery") {
+	  page=1
+	  renderPhotosInTab(gallery)
+  }
+  if (activeTabId === "favorit") {
+	  page=1
+	renderPhotosInTab(favorites)
+
+  }
 }
+
+//function responsible for handling tabs content and some other functionality related to tabs
+function renderPhotosInTab(tabArray) {
+  mainPhotosContainer.innerHTML = "";
+  let activeTabId = activeTab();
+  messageForPhotos.style.display='none'
+  LoadMoreBtn.style.display = 'none';
+  searchBox.style.opacity = 0;
+  renderPhotos(activeTabId, tabArray);
+}
+
+
+
+
+/*
+
+//fucntion responisble for rendering  photos in gallery
+function renderGallery(){
+	 mainPhotosContainer.innerHTML = "";
+	let activeTabId=activeTab()
+	LoadMoreBtn.style.display='none';
+    searchBox.style.opacity = 0;
+    renderPhotos(activeTabId, gallery);
+	
+}
+
+*/
+/*
+//fucntion responisble for rendering  photos in favorites
+function renderFavorite(){
+	  mainPhotosContainer.innerHTML = "";
+	let activeTabId=activeTab()
+	 LoadMoreBtn.style.display='none';
+    searchBox.style.opacity = 0;
+    renderPhotos(activeTabId, favorites);
+	
+}
+*/
 //function responsible for opening spesifice tab based on the id provided by  handltab function
 function openTab(tabId) {
   for (let i = 0; i < tabs.length; i++) {
@@ -50,13 +123,14 @@ function openTab(tabId) {
 //function resposible for handling api
 async function getPhotosFromApi() {
   try {
-    let response = await fetch(`${baseUrl}${query}${key}`);
+	  const baseUrl = `https://api.unsplash.com/search/photos?per_page=12&page=${page}&query=${query}&client_id=ds9Xs3l6PjIhSsb8NrDn9nTPYq1cvz90ZbOkqkS0RKs`;
+    let response = await fetch(`${baseUrl}`);
     let data = await response.json();
 
     let photosArr = data.results;
     return photosArr;
   } catch (error) {
-    console.log(error);
+      return error;
   }
 }
 //function responsible for  searching functionality and rendering photos based on each tabs
@@ -65,33 +139,40 @@ async function searchAndRenderPhotos() {
   query = inputVal.value;
   let newPhotos = await getPhotosFromApi();
   let activeTabId = activeTab();
+ 
 
-  mainPhotosContainer.innerHTML = "";
 
-  if (activeTabId === "main") {
+ if(page===1){
+	  mainPhotosContainer.innerHTML = "";
+ }
+    
+	   if (activeTabId === "main") {
     searchBox.style.opacity = 1;
     newPhotos.forEach((photo) => {
       mainPhotosContainer.innerHTML += generateHtmlForTabs(activeTabId, photo);
     });
   }
+	   
+  if(mainPhotosContainer.innerHTML!==''){
+	   LoadMoreBtn.style.display='block';
+	   messageForPhotos.style.display='none'
+  }
+ hideSpinner();
 
-  if (activeTabId === "gallery") {
-    searchBox.style.opacity = 0;
-    renderPhotos(activeTabId, gallery);
-  }
-  if (activeTabId === "favorit") {
-    searchBox.style.opacity = 0;
-    renderPhotos(activeTabId, favorites);
-  }
 }
 
 //function responsible for renderning photos for gallery and favorite tab
-const renderPhotos = (activeTabId, Photos) => {
-  Photos.forEach((photo) => {
+const renderPhotos = (activeTabId, photos) => {
+
+	if( (photos.length === 0)){
+		  mainPhotosContainer.innerHTML =` <h1 class='add-photos' >Add Photos</h1>`
+		  return;
+	}
+  photos.forEach((photo) => {
     mainPhotosContainer.innerHTML += generateHtmlForTabs(activeTabId, photo);
   });
 
-  hideSpinner();
+ 
 };
 //function responsible for providing the id of active tab
 function activeTab() {
@@ -109,9 +190,13 @@ function generateHtmlForTabs(tabId, photo) {
     return `
             <div class="photo-box">
                 <div class="img">
-                    <img src="${photo.urls.regular}">
+				
+                <a href='${photo.urls.full}' target='_blank'>    <img src="${photo.urls.regular}" title='click to view full image' ></a>
                 </div>
                 <div class="fav-button">
+				
+				<p class='description'>${photo.alt_description}
+</p>
                     <button class="add-to-gal" data-id='${photo.id}' >Add to Gallery</button>
                     <button class="fav" data-id='${photo.id}'>Favorites</button>
                 </div>
@@ -120,9 +205,11 @@ function generateHtmlForTabs(tabId, photo) {
     return `
             <div class="photo-box">
                 <div class="img">
-                    <img src="${photo.url}">
+                   <a href='${photo.full}' target='_blank'>    <img src="${photo.url}" ></a>
                 </div>
                 <div class="fav-button">
+				<p class='description'>${photo.description}
+</p>
                     <button class="remove-from-gal" data-id='${photo.id}'>Remove from Gallery</button>
                     
             </div>`;
@@ -130,9 +217,11 @@ function generateHtmlForTabs(tabId, photo) {
     return `
             <div class="photo-box">
                 <div class="img">
-                    <img src="${photo.url}">
+                    <a href='${photo.full}' target='_blank'>    <img src="${photo.url}" ></a>
                 </div>
                 <div class="fav-button">
+				<p class='description'>${photo.description}
+</p>
                    
                     <button class="unfav" data-id='${photo.id}'>Unfavorite</button>
                 </div>
@@ -141,6 +230,7 @@ function generateHtmlForTabs(tabId, photo) {
 }
 //function responsible for handling all the add remove favorite buttons click
 async function handleAddRemoveClicks(e) {
+	
   if (isLoggedIn) {
     if (e.target.classList.contains("add-to-gal")) {
       let id = e.target.dataset.id;
@@ -159,28 +249,33 @@ async function handleAddRemoveClicks(e) {
       let id = e.target.dataset.id;
 
       gallery = gallery.filter((item) => id !== item.id);
-
+renderPhotosInTab(gallery)
       saveToGallery();
+	  
     }
     if (e.target.classList.contains("unfav")) {
       let id = e.target.dataset.id;
 
       favorites = favorites.filter((item) => id !== item.id);
-
+renderPhotosInTab(favorites)
       saveToFavorite();
+	  
     }
 
-    searchAndRenderPhotos();
   } else {
     alert("login First");
   }
 }
 //function resposible for adding photos to gallery or mark photos as favorite
 function addPhotos(photos, id, photoArray) {
+	if(!photos){
+			return;
+	}
   const matchingItem = photos.find((photo) => photo.id === id);
+	
 
   if (!photoArray.some((element) => matchingItem.id === element.id)) {
-    photoArray.push({ id: matchingItem.id, url: matchingItem.urls.small });
+    photoArray.push({ id: matchingItem.id, url: matchingItem.urls.regular,full:matchingItem.urls.full,description: matchingItem.alt_description });
     saveToGallery();
     saveToFavorite();
   } else {
